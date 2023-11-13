76ea3441a1cc4188a83080d9f46a76a60e9be26e
p2204947 <tom.pellecchia@etu.univ-lyon1.fr>
2023-11-13 02:30:56 +0100

    phase 1 fonction part2

--- Phase1.sql
+++ Phase1.sql
@@ -47,26 +47,6 @@ create table Parametre(
 );
 
 
-drop procedure if exists ajoutMesure;
-delimiter $
-create procedure ajoutMesure (
-   IN idCampagne    int,
-   IN Temperature  	float,
-   IN CO2			float,
-   IN O2			float,
-   IN Lumiere	  	float,
-   IN Humidite		float,
-   IN DateHeure		datetime)
-begin
-insert into Mesure values (idCampagne,Temperature,CO2,O2,Lumiere,Humidite,DateHeure); 
-END $
-DELIMITER ;
-call ajoutMesure(1,null,62,165,15,154,158,now());
-call ajoutMesure(1,null,165,15,154,158,now());
-call ajoutMesure(2,165,null,null,154,158,now());
-call ajoutMesure(2,165,null,null,154,158,now());
-
-
 drop procedure if exists ajoutCampagne;
 delimiter $
 create procedure ajoutCampagne (
@@ -87,6 +67,40 @@ call ajoutCampagne("test1",1,0,1,0,1,100,null,5000);
 call ajoutCampagne("be",1,1,0,0,1,10,500,200);
 call ajoutCampagne("quoi",0,0,1,0,1,500,null,15000);
 call ajoutCampagne("feur",0,0,1,0,1,500,null,15000);
+call ajoutCampagne("feur2",0,0,1,0,1,500,null,15000);
+
+
+drop procedure if exists ajoutMesure;
+delimiter $
+create procedure ajoutMesure (
+   IN idCampagne    int,
+   IN Temperature  	float,
+   IN CO2			float,
+   IN O2			float,
+   IN Lumiere	  	float,
+   IN Humidite		float,
+   IN DateHeure		datetime)
+begin
+insert into Mesure values (idCampagne,Temperature,CO2,O2,Lumiere,Humidite,DateHeure); 
+END $
+DELIMITER ;
+call ajoutMesure(1,null,62,165,14,158,now());
+call ajoutMesure(1,null,65,15,154,25,now());
+call ajoutMesure(2,165,null,null,154,158,now());
+call ajoutMesure(2,165,null,null,154,158,now());
+call ajoutMesure(4,165,null,null,154,158,now());
+
+
+drop procedure if exists supprCampagne;
+delimiter $
+create procedure supprCampagne (IN id  int)
+begin
+delete from Mesure where idCampagne=id;
+delete from CampagneMesure where idCampagne=id;
+END $
+DELIMITER ;
+call supprCampagne(4);
+
 
 drop procedure if exists triCampagne;
 delimiter $
@@ -111,20 +125,43 @@ call rechercheCampagne('1');
 drop procedure if exists exportCampagne;
 delimiter $
 create procedure exportCampagne (
-   IN id	       		int,
-   IN Temperature  		boolean,
-   IN CO2				boolean,
-   IN O2				boolean,
-   IN Lumiere	  		boolean,
-   IN Humidite			boolean,
-   IN intervalReleve	int,
-   IN moyenne			boolean,
-   IN Debut				datetime,
-   IN Fin				datetime,
-   IN volume 			boolean)
+	IN id 					int,
+    IN capteurTemperature  	boolean,
+	IN capteurCO2			boolean,
+	IN capteurO2			boolean,
+	IN capteurLumiere	  	boolean,
+	IN capteurHumidite		boolean,
+    IN debut 				datetime,
+    IN fin 					datetime)
 begin
-select Temperature,CO2,O2,Lumiere,Humidite from Mesure where idCampagne=id; 
+declare requete varchar(500);
+set @requete ="SELECT ";
+
+if capteurTemperature then
+	set @requete=concat(requete,"Temperature,");
+end if;
+if capteurCO2 then
+	set @requete=concat(requete,"CO2,");
+end if;
+if capteurO2 then
+	set @requete=concat(requete,"O2,");
+end if;
+if capteurLumiere then
+	set @requete=concat(requete,"Lumiere,");
+end if;
+if capteurHumidite then
+	set @requete=concat(requete,"Humidite,");
+end if;
+
+set @requete=concat(requete,"DateHeure from Mesure where idCampagne=",id," and DateHeure<",fin," and DateHeure>",debut,";");
+
+prepare exportation from @requete;
+execute exportation;
+deallocate prepare exportation;
+
+/*select Temperature,CO2,O2,Lumiere,Humidite from Mesure where idCampagne=id and DateHeure<fin and DateHeure>debut; 
+select @requete;*/
 END $
 DELIMITER ;
+call exportCampagne(2,1,1,1,0,0,'2023-11-10 11:30:10',now()); 
 
-call exportCampagne(1,1,1,1,0,0,65,0,'2023-11-10 11:30:10','2023-11-10 11:30:10',0); 
