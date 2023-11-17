ccce70e573471f71a45568c03c8182c19f069cb4
NicoAppsDev <contact.nicoappsdev@free.fr>
2023-11-17 11:31:17 +0100

    loader fix

--- index.php
+++ index.php
@@ -51,7 +51,9 @@
 
             <div class="liste_CM" id="CM_container">
                 <div class="loading_popup" id="loading_div">
-                    <progress class="pure-material-progress-circular"></progress>
+                    <svg class="spinner" viewBox="0 0 50 50">
+                        <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+                    </svg>
                     <p class="loading_msg">Récupération des campagnes...</p>
                 </div>
             </div>
