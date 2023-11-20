3f8aa49aa15d809611dce160b24c1582724e5c85
Nicolas COLOMB <nicolas.colomb@etu.univ-lyon1.fr>
2023-11-20 18:33:45 +0100

    début fix UI confirm + error

--- js/functions.js
+++ js/functions.js
@@ -6,11 +6,9 @@ async function displayConfirm(title, msg, confirmBtnTitle, destructive = false) 
     const id = id_confirm;
     id_confirm++;
 
-
-
     const html_popup = `
-        <div class="popup open_anim" id="confirm_popup_container${id}">
-            <div class="popup-inner blur">
+        <div class="popup visible" id="confirm_popup_container${id}">
+            <div id="confirm_popup${id}" class="popup-inner blur">    
                 <div class="popup-content center">
                     <img src="./img/warning_ico.svg" class="ico_msgbox">
                     <p class="title_msgbox">${title}</p>
@@ -27,6 +25,10 @@ async function displayConfirm(title, msg, confirmBtnTitle, destructive = false) 
 
     document.body.insertAdjacentHTML("beforeend", html_popup);
 
+    setTimeout(() => {
+        document.getElementById("confirm_popup_container" + id).style.opacity = 1;
+        document.getElementById("confirm_popup" + id).style.transform = "scale(1)";
+    }, 10);
 
     const promise = new Promise((resolve, reject) => {
         document.getElementById("confirm" + id).addEventListener('click', resolve)
@@ -47,12 +49,13 @@ async function displayConfirm(title, msg, confirmBtnTitle, destructive = false) 
 
 async function hideConfirm(id) {
     const popup_container = document.getElementById("confirm_popup_container" + id);
-    popup_container.classList.remove("open_anim");
-    popup_container.classList.add("close_anim");
+
+    popup_container.style.opacity = 0;
+    document.getElementById("confirm_popup" + id).style.transform = "scale(0.95)";
 
     setTimeout(function() {
         popup_container.remove();
-    }, 500);
+    }, 250);
 }
 
 async function displayError(title, msg) {
@@ -60,25 +63,15 @@ async function displayError(title, msg) {
     id_error++;
 
     const html_popup = `
-        <div class="popup open_anim" id="error_popup_container${id}">
-            <div class="card inner-error-popup">
-                <div class="card-header">
-                    <b class="card-title">Erreur :(</b>
-                    <div class="status-actions">
-                        <div class="status-action-container stop" onclick="hideError(${id})">
-                            <svg class="action-icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0,0,256,256"><g transform="translate(-53.76,-53.76) scale(1.42,1.42)"><g fill="currentColor" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.33333,5.33333)"><path transform="translate(24.00059,-9.94113) rotate(45.001)" d="M21.5,4.5h5.001v39h-5.001z"></path><path transform="translate(57.94113,24.00474) rotate(135.008)" d="M21.5,4.5h5v39.001h-5z"></path></g></g></g></svg>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="status-row">
-                        <div class="status-title">
-                            <img style="width: 16px;" class="status-icon" src="./img/error_status.svg">
-                            <span id="error_title">${title}</span>
-                        </div>
-                        <span class="status-message" id="error_msg">
-                            ${msg.replace("\n", "<br>")}
-                        </span>
+        <div class="popup visible" id="error_popup_container${id}">
+            <div id="error_popup${id}" class="popup-inner blur">    
+                <div class="popup-content center">
+                    <img src="./img/error_ico.svg" class="ico_msgbox">
+                    <p class="title_msgbox">${title}</p>
+                    <p class="msg_msgbox">${msg}</p>
+                    
+                    <div class="actions_msgbox">
+                        <button id="close${id}" class="rect_round_btn gray" type="button">OK</button>
                     </div>
                 </div>
             </div>
@@ -86,16 +79,33 @@ async function displayError(title, msg) {
     `;
 
     document.body.insertAdjacentHTML("beforeend", html_popup);
+
+    setTimeout(() => {
+        document.getElementById("error_popup_container" + id).style.opacity = 1;
+        document.getElementById("error_popup" + id).style.transform = "scale(1)";
+    }, 10);
+
+    const promise = new Promise((resolve) => {
+        document.getElementById("close" + id).addEventListener('click', resolve)
+    });
+
+
+    return await promise
+    .then(() => {
+        hideError(id);
+        return;
+    })
 }
 
 async function hideError(id) {
     const popup_container = document.getElementById("error_popup_container" + id);
-    popup_container.classList.remove("open_anim");
-    popup_container.classList.add("close_anim");
+
+    popup_container.style.opacity = 0;
+    document.getElementById("error_popup" + id).style.transform = "scale(0.95)";
 
     setTimeout(function() {
         popup_container.remove();
-    }, 500);
+    }, 250);
 }
 
 async function displayLoading(msg = "Chargement...") {
