2210b77eead44ac18a060b76b7a5faecdbc00090
NicolasAppsDevelopment <colomb.nicolas@free.fr>
2024-01-21 15:21:52 +0100

    verif running campaign before restart

--- PHP_API/restart_campaign.php
+++ PHP_API/restart_campaign.php
@@ -18,9 +18,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         replyError("Impossible de redémarrer la campagne", "Le format de l'identifiant de la campagne est incorrecte. Veuillez rafraîchir la page puis réessayer.");
     }
 
+    // check if a campaign is already running
+    $url = "http://192.168.4.1:1880/check_working_campaign";
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $res = curl_exec($curl);
+    $info = curl_getinfo($curl);
+    curl_close($curl);
+
+    if ($info["http_code"] != 200) {
+        if ($info["http_code"] == 500) {
+            replyError("Impossible de redémarrer la campagne", "Une campagne est déjà en cours d'exécution. Veuillez attendre la fin de celle-ci ou arrêtez la puis réessayer.");
+        }
+        replyError("Impossible de redémarrer la campagne", "Une erreur est survenue lors de la vérification de l'état de la campagne en cours d'exécution. Veuillez réessayer.");
+    }
+
     reply(array(
         "success" => restartCampaign($id)
     ));
 } else {
-    replyError("Impossible de redémarrer le contenu de la campagne", "La méthode de requête est incorrecte.");
+    replyError("Impossible de redémarrer la campagne", "La méthode de requête est incorrecte.");
 }
