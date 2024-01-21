533913e2bae357a4b936be3ffe54f5c0aacbe465
NicolasAppsDevelopment <colomb.nicolas@free.fr>
2024-01-21 22:31:42 +0100

    final fix check restart

--- PHP_API/restart_campaign.php
+++ PHP_API/restart_campaign.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!array_key_exists("idCurrent", $data)) {
         replyError("Impossible de redémarrer la campagne", "Une erreur est survenue lors de la vérification de l'état de la campagne en cours d'exécution. Veuillez réessayer.");
     }
-    if ($data["idCurrent"] != null || $data["idCurrent"] != $id) {
+    if ($data["idCurrent"] != null && $data["idCurrent"] != $id) {
         replyError("Impossible de redémarrer la campagne", "Une campagne est déjà en cours d'exécution. Veuillez attendre la fin de celle-ci ou arrêtez la puis réessayer.");
     }
 
