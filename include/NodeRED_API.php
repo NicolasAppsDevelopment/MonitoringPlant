6c0ab80fe582a16130eadaf78afc9148f2986af8
PELLECCHIA TOM p2204947 <tom.pellecchia@etu.univ-lyon1.fr>
2024-02-14 15:55:18 +0000

    Update file NodeRED_API.php

--- include/NodeRED_API.php
+++ include/NodeRED_API.php
@@ -17,7 +17,7 @@ function NodeRedPost(string $name, array $array)
 
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_URL, $requestUrl);
+    curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($array));
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
