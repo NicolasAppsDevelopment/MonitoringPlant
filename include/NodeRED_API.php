b732d7fa1e1d34bcac945f764afc83e1add33611
PELLECCHIA TOM p2204947 <tom.pellecchia@etu.univ-lyon1.fr>
2024-02-14 15:33:41 +0000

    fix noderedpost + noderedget

--- include/NodeRED_API.php
+++ include/NodeRED_API.php
@@ -9,13 +9,13 @@ $NODE_RED_API_URL = "http://$NODE_RED_API_IP:$NODE_RED_API_PORT";
  * This function send a post request to Node Red which reply.
  * @param  string $name the name of the Node Red request will receive. This string must not contains any special characters.
  * @param array $array the parameters that you want to transmir to Node Red
+ * @return values  $res is the data that NodeRed send back, it can be a interger, string, etc... 
  */
 function NodeRedPost(string $name, array $array)
 {
-    //$NodeRedUrl = 'http://192.168.4.1:1880';
+    $url = "$NODE_RED_API_URL/$name";
 
-    $requestUrl = $NODE_RED_API_URL . "/" . $name;
-    $curl = curl_init($requestUrl);
+    $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_URL, $requestUrl);
     curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($array));
@@ -23,6 +23,23 @@ function NodeRedPost(string $name, array $array)
 
     $res = curl_exec($curl);
     curl_close($curl);
-    return $res;
+    return json_decode($res, true);
 
+}
+
+/**
+ * This function send a get request to Node Red which reply.
+ * @param  string $name the name of the Node Red request will receive. This string must not contains any special characters.
+ * @return values  $res is the data that NodeRed send back, it can be a interger, string, etc... 
+ */
+function NodeRedGet(string $name) {
+    $url = "$NODE_RED_API_URL/$name";
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $res = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($res, true);
 }
