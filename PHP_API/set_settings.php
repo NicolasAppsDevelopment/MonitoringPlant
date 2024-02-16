fb902b0743940ae9f824d0a458d9c6d21a4cd562
PELLECCHIA TOM p2204947 <tom.pellecchia@etu.univ-lyon1.fr>
2024-02-16 16:04:09 +0000

    Update 2 files
    - /PHP_API/set_settings.php
    - /js/parametre.js

--- PHP_API/set_settings.php
+++ PHP_API/set_settings.php
@@ -51,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             break;
     }
 
-    if($arguments["network"]!=null && $arguments["network"]!=NodeRedGet("getAccesPoint")){
+    if($arguments["network"]!=null && $arguments["network"]!=NodeRedGet("getAccessPoint")){
         if(strlen($arguments["network"])<=32 && strlen($arguments["network"])>0){
             if(preg_match($arguments["network"],/^[a-zA-Z0-9\s-_]+$/)){
                 NodeRedPost("setAccesPoint",array('network' => $arguments["network"]));
