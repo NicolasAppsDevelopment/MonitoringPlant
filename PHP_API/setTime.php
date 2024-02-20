6240e69f05e69ff14b89ecbe32caa3da2c7ecb85
PELLECCHIA TOM p2204947 <tom.pellecchia@etu.univ-lyon1.fr>
2024-02-20 23:17:39 +0000

    Update file setTime.php

--- PHP_API/setTime.php
+++ PHP_API/setTime.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     reply(
-        NodeRedPost("setDatetime", $arguments["datetime"])
+        NodeRedPost("setDatetime", array('datetime' => $arguments["datetime"]))
     );
 
 } else {
