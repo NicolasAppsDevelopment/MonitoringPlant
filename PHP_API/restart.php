1ef9d928ca17ae7b525b96ae4356b84d9dfbd7bd
tom pellecchia <tom.pellecchia1@gmail.com>
2024-02-23 18:07:23 +0100

    include node into restart.php

--- PHP_API/restart.php
+++ PHP_API/restart.php
@@ -3,11 +3,11 @@ header("Content-Type: application/json; charset=utf-8");
 
 include_once __DIR__ . "/../include/database.php";
 include_once __DIR__ . "/../include/reply.php";
+include_once __DIR__ . "/../include/NodeRED_API.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // handle GET request
     reply(NodeRedPost('restart',array('key' =>"I_do_believe_I_am_on_fire")));
-
 } else {
     replyError("Impossible de redémarrer le microcontrôleur", "La méthode de requête est incorrecte.");
 }
