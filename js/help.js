d3c3708c2780b938ae979ed7dd1ee0441e0abd31
iutbgdin <iutbgdin@iutabgdinvm-123.iutbourg.univ-lyon1.fr>
2024-03-28 12:42:31 +0100

    grid placed (fix switch in JS to display popup)

--- js/help.js
+++ js/help.js
@@ -21,8 +21,7 @@ async function loadPopUp(id){
     console.log(id);
     event.stopPropagation();
 
-    const helpPopup = document.getElementById("popUpAide");
-    const display = document.getElementById("help-popup");
+    let helpPopup = document.getElementById("popUpAide");
 
     let title;
     let imgLoad;
@@ -30,39 +29,39 @@ async function loadPopUp(id){
     let nbImg;
 
     switch(id){
-        case "helpIndexArticle1":
+        case "helpCampaignArticle1":
             title="Comment créer une nouvelle campagne de mesure ?";
             imgLoad="filtrageHelp.gif";
             description="Vous pouvez filtrez par date.";
             nbImg=2;
             break;
-        case "helpIndexArticle2":
-                title="Comment filtrer une campagne de mesure ?";
-                imgLoad="createCampaign.gif";
-                description="Durée : définit la durée d'une campagne de mesure. Vous pouvez changez l'unité de mesure en minute heure ou jour"+
-                "\n Intervalle: ce paramètre sert à définir l'intervalle entre chaque prise de mesure.";
-                nbImg=4;
-                break;
-        case "helpSettingsArticle1":
-                title="Filtrage";
-                imgLoad="filtrageHelp.gif";
-                description="Vous pouvez filtrez par date.";
-                nbImg=2;
-                break;
-        case "helpSettingsArticle2":
-                title="Comment changer le nom du Wifi ?";
-                imgLoad="filtrageHelp.gif";
-                description="Vous pouvez filtrez par date.";
-                nbImg=2;
-                break;
-        case "helpSeeDataArticle1":
-                title="Comment voir mes mesures sur Excel ?";
-                imgLoad="filtrageHelp.gif";
-                description="Vous pouvez filtrez par date.";
-                nbImg=2;
-                break;
-        case "helpSeeDataArticle2":
+        case "helpCampaignArticle2":
+            title="Comment filtrer une campagne de mesure ?";
+            imgLoad="createCampaign.gif";
+            description="Durée : définit la durée d'une campagne de mesure. Vous pouvez changez l'unité de mesure en minute heure ou jour"+
+            "\n Intervalle: ce paramètre sert à définir l'intervalle entre chaque prise de mesure.";
+            nbImg=4;
+            break;
+        case "helpCampaignArticle3":
+            title="Comment voir mes mesures sur Excel ?";
+            imgLoad="filtrageHelp.gif";
+            description="Vous pouvez filtrez par date.";
+            nbImg=2;
+            break;
+        case "helpCampaignArticle4":
             title="Comment redémmarer ma campagne ?";
+            imgLoad="filtrageHelp.gif";
+            description="Vous pouvez filtrez par date.";
+            nbImg=2;
+            break;
+        case "helpSettingsArticle1":
+            title="Filtrage";
+            imgLoad="filtrageHelp.gif";
+            description="Vous pouvez filtrez par date.";
+            nbImg=2;
+            break;
+        case "helpSettingsArticle2":
+            title="Comment changer le nom du Wifi ?";
             imgLoad="filtrageHelp.gif";
             description="Vous pouvez filtrez par date.";
             nbImg=2;
@@ -73,50 +72,51 @@ async function loadPopUp(id){
             description="Vous pouvez filtrez par date.";
             nbImg=2;
             break;
+        case "helpConnexion":
+            title="Connexion avec les questions de sécurité";
+            imgLoad="connexionHelp.gif";
+            description="Vous pouvez vous connectez grâce à vos questions de sécurité.";
+            nbImg=2;
+            break;
         case "":
             break;
     }
 
-    helpPopup.innerHTML = `
+    helpPopup.innerHTML = `<p>${id}</p>`;
+
+    /*helpPopup.innerHTML = `
 
     <div class="popupHelp popup">
-            <div class="popup-inner">
-                <div class="popup-title">
-                    <p>`+title+`</p>
-                    <label for="help-popup" class="round_btn transparent small close" onclick="loadPopUp()"></label>
-                </div>
-                <img id="HelpGif" src="/img/clock.svg" alt="image d'aide">
-                <p> `+description+` </p>
-                <div class="buttonPopupHelp">
-                    <button class="rect_round_btn" type="button" onclick="previousGif()">
-                        Previous
-                    </button>
-                    <input id="idHelpIndex" hidden value=`+nbImg+`></input>
-                    <button class="rect_round_btn" type="button" onclick="nextGif()"> 
-                        Next 
-                    </button>
-                </div>
+        <div class="popup-inner">
+            <div class="popup-title">
+                <p>`+title+`</p>
+                <label for="help-popup" class="round_btn transparent small close" onclick="loadPopUp()"></label>
             </div>
-    `;
-    if(display.value==1){
-        display.value=0;
-        helpPopup.style.display="flex";
+            <img id="HelpGif" src="/img/clock.svg" alt="image d'aide">
+            <p> `+description+` </p>
+            <div class="buttonPopupHelp">
+                <button class="rect_round_btn" type="button" onclick="previousGif()">
+                    Previous
+                </button>
+                <input id="idhelpCampaign" hidden value=`+nbImg+`></input>
+                <button class="rect_round_btn" type="button" onclick="nextGif()"> 
+                    Next 
+                </button>
+            </div>
+        </div>
+    `;*/
+    
 
-    }else{
-        display.value=1;
-        helpPopup.style.display="none";
-
-    }
     console.log("click");
 
 }
 
 function nextGif(){
-    let id=document.getElementById("idHelpIndex").value;
+    let id=document.getElementById("idhelpCampaign").value;
     console.log(id);
     if(id<3){
         const nb=parseInt(id)+1;
-        document.getElementById("idHelpIndex").value=nb;
+        document.getElementById("idhelpCampaign").value=nb;
 
         switch (id) {
             case 1:
@@ -139,11 +139,11 @@ function nextGif(){
 }
 
 function previousGif(){
-    let id=document.getElementById("idHelpIndex");
+    let id=document.getElementById("idhelpCampaign");
 
     if(id>1){
         id--;
-        document.getElementById("idHelpIndex").value=id;
+        document.getElementById("idhelpCampaign").value=id;
 
         switch (id) {
             case 1:
