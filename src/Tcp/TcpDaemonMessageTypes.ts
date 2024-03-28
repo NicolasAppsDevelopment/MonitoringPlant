789218b335429aa049b199e1b926ca15e0d4703f
NicolasAppsDevelopment <colomb.nicolas@free.fr>
2024-03-28 16:09:19 +0100

    gestion des erreurs du module de mesure TCP

--- src/Tcp/TcpDaemonMessageTypes.ts
+++ src/Tcp/TcpDaemonMessageTypes.ts
@@ -1,18 +1,31 @@
 import { nanoid } from 'nanoid';
 
+export class TcpDaemonAnswerError extends Error {
+    code: number;
+    
+    constructor(code: number, message: string) {
+        super(message);
+        this.code = code;
+    }
+}
+
+
 export class TcpDaemonAnswer {
     response: any;
     id: string;
-    error: boolean;
-    
+    success: boolean;
+    error: TcpDaemonAnswerError | null;
+
     constructor(message: string) {
         let answer = JSON.parse(message);
         this.id = answer.id;
-        this.error = !answer.success;
-        if (this.error) {
-            this.response = answer.error;
+        this.success = answer.success;
+        if (!this.success) {
+            this.error = new TcpDaemonAnswerError(answer.error.code, answer.error.message);
+            this.response = null;
         } else {
             this.response = answer.data;
+            this.error = null;
         }
     }
 }
