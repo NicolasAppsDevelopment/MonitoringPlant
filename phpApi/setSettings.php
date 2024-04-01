e39c6c0514f89ff522ef0e4eaa4702f7632e2f46
NicolasAppsDevelopment <colomb.nicolas@free.fr>
2024-04-01 22:14:59 +0200

    set settings api fix php

--- phpApi/setSettings.php
+++ phpApi/setSettings.php
@@ -53,18 +53,18 @@ try {
         $networkData = NodeJsGet("getAccessPoint")["data"];
         $setAccessPointArgs = array();
         if ($arguments["ssid"]!=null) {
-            if(strlen($arguments["ssid"]) > 32 && strlen($arguments["ssid"]) == 0){
+            if(strlen($arguments["ssid"]) > 32 || strlen($arguments["ssid"]) == 0){
                 throw new Exception("Le nouveau nom du réseau dépasse 32 caractères ou ne contient aucun caractère. Veuillez renseigner un nom de réseau entre 1 et 32 caractères.");
             }
             if(!preg_match('/^[a-zA-Z0-9\s\-_]+$/', $arguments["ssid"])){
                 throw new Exception("Des caractères spéciaux et interdits sont utilisés pour le nouveau nom du réseau. Veuillez renseigner un nom de réseau sans caractère spéciaux puis réessayez.");
             }
-            if ($arguments["ssid"]!=$networkData["ssid"]) {
+            if ($arguments["ssid"] != $networkData["ssid"]) {
                 $setAccessPointArgs["ssid"] = $arguments["ssid"];
             }
         }
-        if ($arguments["password"]!=null && $arguments["password"]!=$networkData["password"]) {
-            if(strlen($arguments["ssid"]) < 8){
+        if ($arguments["password"] != null && $arguments["password"] != $networkData["password"]) {
+            if(strlen($arguments["password"]) < 8){
                 throw new Exception("Le mot de passe du réseau doit contenir au moins 8 caractères. Veuillez renseigner un mot de passe de 8 caractères ou plus puis réessayez.");
             }
             $setAccessPointArgs["password"] = $arguments["password"];
