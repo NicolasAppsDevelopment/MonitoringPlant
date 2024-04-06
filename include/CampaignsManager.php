5d08fe8a836342de6be484f122e7632674c9407a
NicolasAppsDevelopment <colomb.nicolas@free.fr>
2024-04-06 17:13:57 +0200

    undo fix

--- include/CampaignsManager.php
+++ include/CampaignsManager.php
@@ -251,9 +251,17 @@ class CampaignsManager {
 
         //Removal of logs
         $this->logsManager->supprLogs($id);
+        
+        //Update campaign start and end dates
+        try {
+            $this->db->fetchAll("UPDATE Campaigns SET beginDate=NOW(), endingDate=DATE_ADD(NOW(),INTERVAL duration SECOND) WHERE idCampaign = :varId", [
+                'varId' => $id
+            ]);
+            return true;
+        } catch (\Throwable $th) {
+            throw new Exception("Impossible de redémarrer la campagne. {$th->getMessage()}");
+        }
 
-        // Update ending date
-        // Managed by NodeJS
     }
 
     /**
