6454b63013e002421175a33e43bb8a8624adba3f
NicolasAppsDevelopment <colomb.nicolas@free.fr>
2024-04-07 18:10:25 +0200

    help page style improvements + favicon update

--- help.php
+++ help.php
@@ -1,70 +1,170 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/x-icon" href="/favicon.ico">
-        <link href="./css/style.css" rel="stylesheet">
-        <title>Aide</title>
-        <script src="./js/help.js"></script>
 
-        <link rel="preload" href="./img/error_ico.svg" as="image"/>
-        <link rel="preload" href="./img/warning_ico.svg" as="image"/>
-    </head>
-    <body class="bg main_theme">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link href="./css/style.css" rel="stylesheet">
+    <title>Aide</title>
+    <script src="./js/help.js"></script>
 
-        <!-- Navigation -->
-        <?php include "modules/header.php";?>
-        <main>
+    <link rel="preload" href="./img/error_ico.svg" as="image" />
+    <link rel="preload" href="./img/warning_ico.svg" as="image" />
+</head>
 
-            <div class="top_action_menu">
-                <!-- Search bar -->
-                <input type="text" placeholder="Rechercher..." class="custom_search_bar" id="campaign_name_search_bar" onkeydown="handleKeyPressSearchBar(event)">
+<body class="bg main_theme">
+
+    <!-- Navigation -->
+    <?php include "modules/header.php"; ?>
+    <main>
+        <div class="row-help-container">
+            <!-- Aside table of contents -->
+            <div class="tablelist-container">
+                <ul class="tablelist">
+                    <li class="tablelist-item-container">
+                        <button id="btn-campaigns" class="tablelist-item-btn active" onclick="showHelpPanel('campaigns')">
+                            <i class="tablelist-item-btn-ico campaign"></i>
+                            <span>Campagnes</span>
+                        </button>
+                    </li>
+                    <li class="tablelist-item-container">
+                        <button id="btn-settigns" class="tablelist-item-btn" onclick="showHelpPanel('settigns')">
+                            <i class="tablelist-item-btn-ico settings"></i>
+                            <span>Paramètres</span>
+                        </button>
+                    </li>
+                    <li class="tablelist-item-container">
+                        <button id="btn-calibrations" class="tablelist-item-btn" onclick="showHelpPanel('calibrations', this)">
+                            <i class="tablelist-item-btn-ico calibration"></i>
+                            <span>Calibrations</span>
+                        </button>
+                    </li>
+                    <li class="tablelist-item-container">
+                        <button id="btn-login" class="tablelist-item-btn" onclick="showHelpPanel('login', this)">
+                            <i class="tablelist-item-btn-ico login"></i>
+                            <span>Connexion</span>
+                        </button>
+                    </li>
+                </ul>
             </div>
 
-            <!-- Grid of Helps -->
-            <div class="grid">
-                <div class="helpSummary" onclick="unroll('helpCampaign', event)">
-                    <div class="preview">
-                        <h4>Fonctionnalités Campagnes</h4>
-                        <span>4 Articles</span>
+            <!-- Content -->
+            <div class="tab-pane-container">
+                <div class="tab-pane active" id="campaigns">
+                    <div class="tab-pane-header">
+                        <div>
+                            <span class="tab-pane-header-ico-container">
+                                <i class="tab-pane-header-ico campaign"></i>
+                            </span>
+                        </div>
+                        <div>
+                            <h4 class="tab-pane-header-title">
+                                Campagnes
+                            </h4>
+                            <span class="tab-pane-header-desc">Obtenez de l'aide sur la gestion des campagnes.</span>
+                        </div>
                     </div>
-                    <ul class="summaryElement" id="helpCampaign">
-                        <li onclick="goToHelpPage(0, event)">Comment créer une nouvelle campagne de mesure ?</label>
-                        <li onclick="goToHelpPage(1, event)">Comment filtrer une campagne de mesure ? </label>
-                        <li onclick="goToHelpPage(2, event)">Comment avoir mes mesures sur Excel ?</label>
-                        <li onclick="goToHelpPage(3, event)">Comment redémmarer ma campagne ?</label>
-                    </ul>
+                    <div>
+                        <a class="help-question-card" onclick="goToHelpPage(0, event)">
+                            <h2 class="help-question-card-title">
+                                Comment créer une nouvelle campagne de mesure ?
+                            </h2>
+                        </a>
+                        <a class="help-question-card" onclick="goToHelpPage(1, event)">
+                            <h2 class="help-question-card-title">
+                            Comment filtrer une campagne de mesure ?
+                            </h2>
+                        </a>
+                        <a class="help-question-card" onclick="goToHelpPage(2, event)">
+                            <h2 class="help-question-card-title">
+                                Comment avoir mes mesures sur Excel ?
+                            </h2>
+                        </a>
+                        <a class="help-question-card" onclick="goToHelpPage(3, event)">
+                            <h2 class="help-question-card-title">
+                                Comment redémmarer ma campagne ?
+                            </h2>
+                        </a>
+                    </div>
                 </div>
-                <div class="helpSummary" onclick="unroll('helpSettings', event)">
-                    <div class="preview">
-                        <h4>Fonctionnalités Paramètres</h4>
-                        <span>2 Articles</span>
+
+                <div class="tab-pane" id="settigns">
+                    <div class="tab-pane-header">
+                        <div>
+                            <span class="tab-pane-header-ico-container">
+                                <i class="tab-pane-header-ico settings"></i>
+                            </span>
+                        </div>
+                        <div>
+                            <h4 class="tab-pane-header-title">
+                                Paramètres
+                            </h4>
+                            <span class="tab-pane-header-desc">Obtenez de l'aide sur les paramètres de l'appareil.</span>
+                        </div>
                     </div>
-                    <ul class="summaryElement" id="helpSettings">
-                        <li onclick="goToHelpPage(4, event)" >Comment marche la Suppression automatique ?</label>
-                        <li onclick="goToHelpPage(5, event)">Comment changer le nom du Wi-Fi ?</label>
-                    </ul>
+                    <div>
+                        <a class="help-question-card" onclick="goToHelpPage(4, event)">
+                            <h2 class="help-question-card-title">
+                                Comment marche la suppression automatique ?
+                            </h2>
+                        </a>
+                        <a class="help-question-card" onclick="goToHelpPage(5, event)">
+                            <h2 class="help-question-card-title">
+                                Comment changer le nom du Wi-Fi ?
+                            </h2>
+                        </a>
+                    </div>
                 </div>
-                <div  class="helpSummary" onclick="unroll('helpCalibrate', event)">
-                    <div class="preview">
-                        <h4>Fonctionnalités Calibrations</h4>
-                        <span>1 Article</span>
+
+                <div class="tab-pane" id="calibrations">
+                    <div class="tab-pane-header">
+                        <div>
+                            <span class="tab-pane-header-ico-container">
+                                <i class="tab-pane-header-ico calibration"></i>
+                            </span>
+                        </div>
+                        <div>
+                            <h4 class="tab-pane-header-title">
+                                Calibrations
+                            </h4>
+                            <span class="tab-pane-header-desc">Obtenez de l'aide sur la gestion de vos calibrations.</span>
+                        </div>
                     </div>
-                    <ul class="summaryElement" id="helpCalibrate">
-                        <li onclick="goToHelpPage(6, event)">Comment enregistrer une nouvelle calibration ?</label>
-                    </ul>
-                </div>  
-                <div  class="helpSummary" onclick="unroll('helpConnexion', event)">
-                    <div class="preview">
-                        <h4>Fonctionnalités Connexion</h4>
-                        <span>1 Article</span>
+                    <div>
+                        <a class="help-question-card" onclick="goToHelpPage(6, event)">
+                            <h2 class="help-question-card-title">
+                                Comment enregistrer une nouvelle calibration ?
+                            </h2>
+                        </a>
                     </div>
-                    <ul class="summaryElement" id="helpConnexion">
-                        <li onclick="goToHelpPage(7, event)">Comment se connecter avec les questions de sécurité ?</label>
-                    </ul>
+                </div>
+
+                <div class="tab-pane" id="login">
+                    <div class="tab-pane-header">
+                        <div>
+                            <span class="tab-pane-header-ico-container">
+                                <i class="tab-pane-header-ico login"></i>
+                            </span>
+                        </div>
+                        <div>
+                            <h4 class="tab-pane-header-title">
+                                Connexion
+                            </h4>
+                            <span class="tab-pane-header-desc">Obtenez de l'aide sur la connexion à votre espace administrateur.</span>
+                        </div>
+                    </div>
+                    <div>
+                        <a class="help-question-card" onclick="goToHelpPage(7, event)">
+                            <h2 class="help-question-card-title">
+                                Comment se connecter avec les questions de sécurité ?
+                            </h2>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </main>
-    </body>
+        </div>
+    </main>
+</body>
+
 </html>
--- js/help.js
+++ js/help.js
@@ -24,3 +24,18 @@ async function goToHelpPage(pageNumber, e){
     window.open('docViewer/web/viewer.html#page=' + pageNumber, '_blank');
 }
 
+/**
+ * Displays the right help panel when the user clicks on a topic according to the id of the panel
+ * @param {number} panelId id of the panel that we want to display
+ */
+async function showHelpPanel(panelId) {
+    let currentTopicsBtn = document.querySelector('.tablelist-item-btn.active');
+    currentTopicsBtn.classList.remove('active');
+    let currentPanel = document.querySelector('.tab-pane.active');
+    currentPanel.classList.remove('active');
+
+    let panel = document.getElementById(panelId);
+    let topicsBtn = document.getElementById('btn-' + panelId);
+    panel.classList.add('active');
+    topicsBtn.classList.add('active');
+}
